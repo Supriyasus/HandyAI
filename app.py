1243139ece9cd662c811128c91ebb89cbf212f2f
Supriya <supriyasrivastava2004@gmail.com>
2025-04-02 22:59:07 +0530

    Update dockerfile

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def detect():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
